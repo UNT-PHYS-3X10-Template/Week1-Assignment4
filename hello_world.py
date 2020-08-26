28d29b7fcfa6b3a4edac53355b78a00d0b0fbef9
Oliviero Andreussi <olivieroandreussi@hotmail.com>
2020-08-26 10:39:17 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,4 @@
 # These are comments, it is a good idea to always add some to explain what the code is supposed to be doing
-# Week 1 Assigment 1
-# Simple code to print "Hello, World"
+# Week 1 Assigment 4
+# Write a code that computes the hyperbolic sine using three different formulas and numpy functions/constants. 
+# Expected outcome: when testing for x = 2 \pi the results should be 267.74489404101644, 267.74489404101644, and 267.7448940410163
